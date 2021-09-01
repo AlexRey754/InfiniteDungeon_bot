011288f8a5b920caef238c75cff11844844b93b5
AlexRey754 <allo201415@gmail.com>
2021-09-01 21:36:56 +0300

    some models added

--- utils/models.py
+++ utils/models.py
@@ -1,10 +1,12 @@
 from enum import unique
+from warnings import catch_warnings
 from peewee import *
 
 db = SqliteDatabase('db.db')
 
 class BaseModel(Model):
     id = PrimaryKeyField(unique=True)
+    uid = IntegerField()
 
     class Meta:
         database = db
@@ -16,5 +18,23 @@ class Player(BaseModel):
     skill_points = IntegerField()
     current_xp = IntegerField()
     max_xp = IntegerField()
+
+
+class Inventory(BaseModel):
+
+    name = CharField()
+    count = CharField()
+    
+
+class Achievement(BaseModel):
+
+    name = CharField()
+
+class Equipment(BaseModel):
+
+    head = CharField()
+    chest = CharField()
+    legs = CharField()
+    hand = CharField()
 
 
