3259544af27c4534e98503a3321807df8bf0e0df
AlexRey754 <allo201415@gmail.com>
2021-09-01 21:28:26 +0300

    player func pattern

---
+++ entity/player.py
@@ -0,0 +1,40 @@
+def add(uid,name):
+    pass
+def has(uid, item, table):
+    pass
+
+def open_characteristics(uid):
+    pass
+
+def level_up(uid):
+    pass
+
+def get_damaged(enemy,uid):
+    pass
+
+def get_xp(uid):
+    pass
+
+def equip(item,uid):
+    pass
+
+def unequip(item, uid):
+    pass
+
+def open_inventory(uid):
+    pass
+
+def get_item(item,uid):
+    pass
+
+def get_debuff(uid):
+    pass
+
+def add_to_inv(uid):
+    pass
+
+def add_rand_item_to_inv(item_pool,uid):
+    pass
+
+def get_money(count,uid):
+    pass
