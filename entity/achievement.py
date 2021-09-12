82deebf3019fbe30d37a335d07861ec92423618f
AlexRey754 <allo201415@gmail.com>
2021-09-12 14:17:50 +0300

    Add lvl achievement

---
+++ entity/achievement.py
@@ -0,0 +1,14 @@
+"""
+All Achievement that you can get in the game
+"""
+
+from utils.classes import Achievement
+
+# LVL Achievements
+
+lvl_2 = Achievement("Начало положено!","Достчь 2 уровня",money=100)
+lvl_10 = Achievement("Идём дальше!","Достичь 10 уровня",money=400)
+lvl_25 = Achievement("Назад дороги нет!","Достичь 25 уровня",money=1000)
+lvl_50 = Achievement("Хай лвл","Достичь 50 уровня", money= 2500)
+lvl_100 = Achievement("Бесконечность не предел!","Достчиь 100 уровня",money=5000)
+
