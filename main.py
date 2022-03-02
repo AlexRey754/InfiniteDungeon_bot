6c0795602bd77039a87b30405a16f9fa2f3a2f1d
AlexRey754 <allo201415@gmail.com>
2022-03-02 18:01:27 +0200

    master

---
+++ main.py
@@ -0,0 +1,18 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+import asyncio
+
+import time
+
+from config import TOKEN
+
+bot = Bot(token=TOKEN)
+dp=Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(message:types.Message):
+    await message.answer('Привет Друг!')
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
